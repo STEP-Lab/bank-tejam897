304ff3c691b50f169f415a8619bf93fc7fd2b45f
teja m <tejamedida897@gmail.com>
2018-03-28 08:39:32 +0530

    Added method to Transactions class for getting specific type of transactions from whole transactions.

---
+++ library/src/test/java/com/thoughtworks/step/Transactions.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.step;
+
+import java.util.ArrayList;
+
+public class Transactions {
+    private ArrayList<Transaction> transactions;
+
+    public Transactions() {
+        this.transactions = new ArrayList<Transaction>();
+    }
+
+    public ArrayList getTransactions() {
+        return transactions;
+    }
+
+    public CreditTransaction credit(float amount, String creditedBy) {
+        CreditTransaction transaction = new CreditTransaction(amount,creditedBy);
+        transactions.add(transaction);
+        return transaction;
+    }
+
+    public DebitTransaction debit(float amount, String debitedBy) {
+        DebitTransaction transaction = new DebitTransaction(amount,debitedBy);
+        transactions.add(transaction);
+        return transaction;
+    }
+
+    public ArrayList<Transaction> getTransactionsByType(Class<? extends Transaction> type) {
+        ArrayList<Transaction> allTransactions = new ArrayList<>();
+        ArrayList allTrans = getTransactions();
+        int trans;
+        for (trans = 0; trans<allTrans.size(); trans++){
+            if(allTrans.get(trans).getClass().equals(type)){
+                allTransactions.add((Transaction) allTrans.get(trans));
+            }
+        }
+        return allTransactions;
+    }
+}
---
+++ library/src/test/java/com/thoughtworks/step/TransactionsTest.java
@@ -0,0 +1,47 @@
+package com.thoughtworks.step;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.junit.Assert.assertThat;
+
+public class TransactionsTest {
+    private Transactions transCollection;
+    private CreditTransaction credit;
+    private DebitTransaction debit;
+    @Before
+    public void setUp() throws Exception {
+        transCollection = new Transactions();
+        credit = transCollection.credit(1000,"teja");
+        debit = transCollection.debit(1000,"teja");
+    }
+
+    @Test
+    public void checksWhetherCreditTransactionsRecordedOrNot() {
+        ArrayList<Transaction> trans = transCollection.getTransactions();
+        assertThat(trans,hasItem (credit));
+    }
+
+    @Test
+    public void checksWhetherDebitTransactionsRecordedOrNot() {
+        ArrayList<Transaction> trans = transCollection.getTransactions();
+        assertThat(trans,hasItem(debit));
+    }
+
+    @Test
+    public void checksCollectingBackOfAllCreditTransactionsFromWholeTransactions() {
+        CreditTransaction TestingCredit = transCollection.credit(1000,"nani");
+        ArrayList<Transaction> creditTrans = transCollection.getTransactionsByType(credit.getClass());
+        assertThat(creditTrans,hasItems(credit,TestingCredit));
+    }
+    @Test
+    public void checksCollectingBackOfAllDebitTransactionsFromWholeTransactions() {
+        DebitTransaction TestingDebit = transCollection.debit(1000,"nani");
+        ArrayList<Transaction> debitTrans = transCollection.getTransactionsByType(debit.getClass());
+        assertThat(debitTrans,hasItems(debit,TestingDebit));
+    }
+}
